483fb13b015016e59f2537548a35d4ee268f5002
SweetConsole <sweetconsole@gmail.com>
2026-05-09 23:19:59 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,15 @@ export default async function Home() {
 
   try {
     stories = await prisma.story.findMany({})
+
+    console.log("True")
+
   } catch (error) {
     console.error('Database connection failed:', error)
     // Во время сборки stories останется пустым массивом
   }
+
+  console.log(stories)
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">

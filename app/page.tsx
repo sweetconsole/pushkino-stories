31ff7c32c5c064b84c5e8b5dc8c5b3497cf45cf4
SweetConsole <sweetconsole@gmail.com>
2026-05-09 23:25:23 +0300

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,14 @@
-"use client"
-import Image from "next/image";
-import {getStories} from "@/api.service"
-import {prisma} from "@/prisma.client"
-import {Story} from "@/app/generated/prisma/client"
+import Stories from "./Stories"
+
 
 export default async function Home() {
-  let stories: Array<Story> = []
 
-  try {
-    stories = await prisma.story.findMany({})
-
-    console.log("True")
-
-  } catch (error) {
-    console.error('Database connection failed:', error)
-    // Во время сборки stories останется пустым массивом
-  }
-
-  console.log(stories)
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Stories</h1>
 
-      <ul>
-        {stories.map((story, index) => (
-          <li key={index}>
-            <h2>{story.title}</h2>
-            <p>{story.description}</p>
-          </li>
-        ))}
-      </ul>
+      <Stories />
     </div>
   );
 }
